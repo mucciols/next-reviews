fd6a66877a151fe0e8bf1c766549ff98daeba496
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-04 20:53:19 +0100

    gome rendering

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 export default function HomePage() {
+  console.log('[home]')
   return(
     <>
       <h1>indie gamer</h1>
       <p>
-        only the best
+        only the best indie games, reviewed for you
       </p>
     </>
   )
