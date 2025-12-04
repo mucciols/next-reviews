5b4752f0279e5f536d8a655de4993743977c8a1b
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-04 19:14:50 +0100

    primo componente di sto cazzo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
-export default function RootLayout() {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-
+        {children}
       </body>
     </html>
-  )
+  );
 }
