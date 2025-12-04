fe186ef2cbca88e17a285199ece91a2c7e493eed
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-04 23:45:21 +0100

    altrocommit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import './global.css'
 
 export default function RootLayout({ children }) {
   return (
@@ -14,16 +15,17 @@ export default function RootLayout({ children }) {
                 <Link href="/reviews">Reviews</Link>
               </li>
               <li>
-                <Link href="/about">About</Link>
+                <Link href="/about" prefetch={false}>About</Link>
               </li>
             </ul>
           </nav>
         </header>
-        <main>
+        <main> 
           {children}
         </main>
         <footer>
-          [footer]
+          Game data and images courtesy 
+          of a <a href="https://rawg.io" target='_blank'>RAWG</a>
         </footer>
       </body>
     </html>
