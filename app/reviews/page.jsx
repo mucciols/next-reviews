b2dba6ec4e0cfc1d1f5ac75fb3715418e574fdbd
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-05 12:58:00 +0100

    item letti da funzione

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -3,37 +3,30 @@ import Heading from "@/components/Heading";
 import { getReviews } from "@/lib/reviews";
 
 export default async function ReviewsPage() {
-  const reviews =  await getReviews();
-  console.log(reviews);
-
+  const reviews = await getReviews();
+  console.log("reiews", reviews);
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white rounded border border-gray-300 shadow w-80 hover:shadow-xl">
-          <Link href="reviews/hollow-knight">
-            <img
-              src="/images/hollow-knight.jpg"
-              className="mb-1 rounded-t"
-              width="640 px"
-              height="360"
-            />
-            <h2 className="font-semibold font-orbitron py-1 text-center">
-              Hollow knight
-            </h2>
-          </Link>
-        </li>
-        <li className="bg-white rounded border border-gray-300 shadow w-80 hover:shadow-xl">
-          <Link href="reviews/stardew-valley">
-            <img
-              src="/images/stardew-valley.jpg"
-              className="mb-1 rounded-t"
-              width="640 px"
-              height="360"
-            />
-            <h2 className="font-semibold font-orbitron py-1 text-center">Stardew Valley</h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li
+            key={review.slug}
+            className="bg-white rounded border border-gray-300 shadow w-80 hover:shadow-xl"
+          >
+            <Link href={`reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                className="mb-1 rounded-t"
+                width="640 px"
+                height="360"
+              />
+              <h2 className="font-semibold font-orbitron py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
