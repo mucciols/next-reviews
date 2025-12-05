279e2d98004d7e6b667c1d24007f01b5301e6668
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-05 11:12:08 +0100

    lettura da files

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,16 +1,17 @@
+import { readFile } from 'node:fs/promises'
 import Heading from "@/components/Heading";
 
-export default function StardewValleyPage() {
+export default async function StardewValleyPage() {
+  const text = await readFile('./content/reviews/stardew-valley.md', 'utf8')
+
   return (
     <>
       <Heading>Stardew valley</Heading>
       <img
-        src="/images/stardew-valley.jpg"
-        className="mb-2 rounded"
-        width="640 px"
-        height="360"
+        src="/images/stardew-valley.jpg" className="mb-2 rounded"
+        width="640 px" height="360"
       />
-      <p>this will be the review for Stardew Valley</p>
+      <p>{text}</p>
     </>
   );
 }
---
+++ content/reviews/stardew-valley.md
@@ -0,0 +1,11 @@
+# Heading
+## Sub-heading
+
+Rich text with __bold__ and_italic_.
+
+Another paragraph.
+
+List:
+* one
+* two
+* three
