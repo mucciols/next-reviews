7fdb5be95ce8178527bf9ff3374872a0cf12513b
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-05 08:53:58 +0100

    immagini

--- app/reviews/hollow-knight/page.jsx
+++ app/reviews/hollow-knight/page.jsx
@@ -4,8 +4,10 @@ export default function HollowKnightPage() {
   return(
     <>
       <Heading>Hollow knight</Heading>
+      <img src="/images/hollow-knight.jpg" className="mb-2 rounded"
+        width="640 px" height="360" />
       <p>
-        knight
+        this will be the review for Hollow Knight
       </p>
     </>
   )
--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -4,6 +4,8 @@ export default function StardewValleyPage() {
   return(
     <>
       <Heading>Stardew valley</Heading>
+      <img src="/images/stardew-valley.jpg" className="mb-2 rounded"
+        width="640 px" height="360" />
       <p>
        valley page
       </p>
