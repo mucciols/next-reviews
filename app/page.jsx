cac9d3446ade33c84d4af4eaf9a134c8b1c775ee
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-05 13:53:44 +0100

    reviews

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getFeaturedReview } from "@/lib/reviews";
 
 export default function HomePage() {
+  
+  const review = getFeaturedReview();
+  
   console.log('[HomePag] rendering');
   return(
     <>
@@ -10,15 +14,15 @@ export default function HomePage() {
         only the best indie games, reviewed for you
       </p>
        <div className="bg-white rounded border border-gray-300 w-80 shadow hover:shadow-xl sm:w-full">
-          <Link href="reviews/stardew-valley" 
+          <Link href={`reviews/${review.slug}`} 
             className="flex flex-col sm:flex-row">
             <img
-              src="/images/stardew-valley.jpg"
+              src={`/images/${review.image}`}
               className="mb-1 rounded-t sm:rounded-l sm:rounded-r-none"
               width="640 px"
               height="360"
             />
-            <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">Stardew Valley</h2>
+            <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">{ review.title }</h2>
           </Link>
         </div>
     </>
