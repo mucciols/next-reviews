c387033856187301e4912c9f4d03d82cff2a1658
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-05 16:54:56 +0100

    copy link

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,33 +1,40 @@
 import Heading from "@/components/Heading";
+import ShareLinkButton from "@/components/ShareLinkButton";
 import { getReview, getSlugs } from "@/lib/reviews";
 
 export async function generateStaticParams() {
   const slugs = await getSlugs();
-  return slugs.map((slug)=> ({ slug }));
+  return slugs.map((slug) => ({ slug }));
 }
 
 export async function generateMetadata({ params }) {
   const { slug } = await params;
   const review = await getReview(slug);
   return {
-    title: review.title
+    title: review.title,
   };
 }
 
-export default async function ReviewPage({ params } ) {
+export default async function ReviewPage({ params }) {
   const { slug } = await params;
   const review = await getReview(slug);
   return (
     <>
       <Heading>{review.title}</Heading>
-      <p className='italic pb-2'>{review.date}</p>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareLinkButton />
+      </div>
       <img
-        src={review.image} className="mb-2 rounded"
-        width="640 px" height="360"
+        src={review.image}
+        className="mb-2 rounded"
+        width="640 px"
+        height="360"
       />
-      <article dangerouslySetInnerHTML={{ __html: review.body}} 
-        className="max-w-screen-sm prose prose-slate">
-      </article>
+      <article
+        dangerouslySetInnerHTML={{ __html: review.body }}
+        className="max-w-screen-sm prose prose-slate"
+      ></article>
     </>
   );
 }
---
+++ components/ShareLinkButton.jsx
@@ -0,0 +1,24 @@
+
+'use client';
+
+import { useState } from "react";
+
+export default function ShareLinkButton() {
+  const [clicked, setClicked] = useState(false);
+  
+  const handleClick = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setClicked(true);
+    setTimeout(() => { setClicked(false) }, 1500);
+  }
+
+  console.log('[ShareLinkButton] clicked', clicked)
+
+  return(
+    <button onClick={handleClick}
+      className="border px-2 py-1 rounded text-slate-500 text-sm 
+            hover:bg-orange-100 hover:text-slate-700">
+      { clicked ? 'Link Copied!' : 'Share Link' } 
+    </button>
+  )
+}
