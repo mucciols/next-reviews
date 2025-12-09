acfb1e7e56e6937b196fc5dc584f8d38daf6d1b7
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-09 20:26:07 +0100

    web hook

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,7 +1,12 @@
+import { CACHE_TAGS_REVIEW } from "@/lib/reviews";
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const payload = await request.json();
+  if(payload.model === 'review') {
+    revalidateTag(CACHE_TAGS_REVIEW);
+  }
   console.log('payload:', payload);
   return new Response(null, { status: 204 });
 }
