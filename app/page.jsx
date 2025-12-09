ad93529c839d738ef1ecd0543e6e09457a11bdc8
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-09 17:31:37 +0100

    Rifattorizzazione delle immagini

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,38 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
 import { getFeaturedReview } from "@/lib/reviews";
+import { getReviews } from "@/lib/reviews";
 
 export default async function HomePage() {
-  const review = await getFeaturedReview();
-  return(
+  const reviews = await getReviews(3);
+  //console.log("[HomePage getReviews 1]", reviews);
+  return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p className="pb-3">
-        only the best indie games, reviewed for you
-      </p>
-       <div className="bg-white rounded border border-gray-300 w-80 shadow hover:shadow-xl sm:w-full">
-          <Link href={`reviews/${review.slug}`} 
-            className="flex flex-col sm:flex-row">
-            <img
-              src={`${review.image}`}
-              className="mb-1 rounded-t sm:rounded-l sm:rounded-r-none"
-              width="640 px"
-              height="360"
-            />
-            <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">{ review.title }</h2>
-          </Link>
-        </div>
+      <p className="pb-3">only the best indie games, reviewed for you</p>
+
+      <ul className="flex flex-col gap-3">
+        {reviews.map((review, index) => (
+          <li key={review.slug}>
+            <div className="bg-white rounded border border-gray-300 w-80 shadow hover:shadow-xl sm:w-full">
+              <Link
+                href={`reviews/${review.slug}`}
+                className="flex flex-col sm:flex-row"
+              >
+                <img
+                  src={`${review.image}`}
+                  className="mb-1 rounded-t sm:rounded-l sm:rounded-r-none"
+                  width="640 px"
+                  height="360"
+                />
+                <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
+                  {review.title}
+                </h2>
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
--- scripts/strapi-reques.mjs
+++ scripts/strapi-reques.mjs
@@ -9,9 +9,9 @@ const url = "http://localhost:1337/api/reviews/"
   populate: { image:{ fields:['url'] } },
   pagination: { pageSize: 1, withCount: false }
 }, { encodeValuesOnly: true })
-console.log(url);
+//console.log(url);
 const response = await fetch(url);
 const body = await response.json();
-console.log(body);
+//console.log(body);
 const file = 'scripts/strapi-response.json';
 //writeFileSync(file, body, 'utf8');
