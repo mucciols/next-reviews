48c2af8785925b2a8de5d7e2f6496a446f2507e9
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-10 11:26:49 +0100

    paginazione

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -14,13 +14,18 @@ export default async function ReviewsPage({ searchParams }) {
   const params = await searchParams;
   const page = parsePageParam(params.page);
 
-  const reviews = await getReviews(PAGE_SIZE, page);
+  const { reviews, pageCount }  = await getReviews(PAGE_SIZE, page);
+  // console.log('rev 1', rev)
+  // const reviews = rev.reviews;
+  // console.log('rev 2', reviews)
+  
+  
   return (
     <>
       <Heading>Reviews</Heading>
       <div>
         <Link href={`/reviews?page=${Number(page) - 1}`}> - </Link>
-        <span> (Page {page}) </span>
+        <span> (Page {page}) of {pageCount} </span>
         <Link href={`/reviews?page=${Number(page) + 1}`}> + </Link>
       </div>
       <ul className="flex flex-row flex-wrap gap-3">
