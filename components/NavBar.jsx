bd32c3a209532133f35e87c3835072c417352143
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-10 14:40:44 +0100

    link disabilitato su stessa url

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,17 +1,26 @@
-import Link from 'next/link'
+import NavLink from './NavLink';
 
 export default function NavBar() {
   return (
     <nav>
       <ul className="flex gap-2">
         <li>
-          <Link className='font-orbitron text-orange-800 hover:underline' href="/">Indie Gamer</Link>
+          {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/">Indie Gamer</Link> */}
+          <NavLink href="/" >Indie Gamer</NavLink>
         </li>
         <li className='ml-auto'>
-          <Link className='font-orbitron text-orange-800 hover:underline' href="/reviews">Reviews</Link>
+          {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/reviews">Reviews</Link> */}
+          <NavLink href="/reviews" >
+            Reviews
+          </NavLink>
         </li>
         <li>
-          <Link className='font-orbitron text-orange-800 hover:underline' href="/about" prefetch={false}>About</Link>
+          {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/about" prefetch={false}>About</Link> */}
+
+          <NavLink href="/about">
+            About
+          </NavLink>
+
         </li>
       </ul>
     </nav>
