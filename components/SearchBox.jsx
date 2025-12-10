7914049b220b68ec62ef2f733d456e18c185e902
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-10 17:13:15 +0100

    search and click

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useIsClient } from "@/lib/hooks";
-import { Combobox, ComboboxOption } from "@headlessui/react";
+import { Combobox } from "@headlessui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const reviews = [
+  { slug: "Hellblade", title: "Hellblade" },
   { slug: "hades-2018", title: "Hades" },
   { slug: "fall-guys", title: "Fall Guys: Ultimate Knockout" },
   { slug: "black-mesa", title: "Black Mesa" },
@@ -13,23 +16,34 @@ const reviews = [
 ];
 
 export default function SearchBox() {
+  const router = useRouter(); 
   const isClient = useIsClient();
+  const [query, setQuery] = useState('');
 
-  // console.log('[SearchBox] isClient:' , isClient);
+  console.log('[SearchBox] query:' , query);
   if (!isClient) {
     return null;
   }
+
+  const handleChange = (review) => {
+    //console.log('[handleChange] review: ' , review);
+    router.push(`/reviews/${review.slug}`)
+    //router.push(`/reviews/Hellblade`)
+  };
+
+  const filteterdReviews = reviews.filter(review=> review.title.includes(query) )
+
   return (
     <div className="relative w-48">
-      <Combobox>
+      <Combobox onChange={handleChange}>
         <Combobox.Input
+          value={query} onChange={ (event) => setQuery(event.target.value) }
           placeholder="Search..."
           className="border px-2 py-1 rounded w-full"
         />
-
         <Combobox.Options className="absolute bg-white py-1 w-full">
-          {reviews.map((review) => (
-            <Combobox.Option key={review.slug} value={review.slug}>
+          {filteterdReviews.map((review) => (
+            <Combobox.Option key={review.slug} value={review}>
               {({ active }) => (
                 <span
                   className={`block px-2 truncate w-full ${
