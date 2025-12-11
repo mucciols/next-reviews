e7e87feadef0159977c88f9f8e8fe4810042eeb1
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-11 17:32:51 +0100

    commenti da db

--- components/CommentList.jsx
+++ components/CommentList.jsx
@@ -1,28 +1,25 @@
 import { UserCircleIcon } from "@heroicons/react/24/outline";
-  import { getComments } from "@/lib/commets";
-
-// const comments = [
-//   {    id: "1", user: "Alice",  message: "Love this game!",  },
-//   {    id: "2", user: "Mark",   message: "Awesome!",  },
-//   {    id: "3", user: "Dennis", message: "Incredible!",  },
-// ];
+import { getComments } from "@/lib/commets";
 
 export default async function CommentList({ slug }) {
-
   const comments = await getComments(slug);
+
+  if (comments.length === 0) {
+    return (<p className="italic mt-3">No Comments yet</p>);
+  }
 
   return (
     <ul className="border mt-3 rounded">
       {comments.map((comment) => (
-        <li key={comment.id}
-          className="border-b px-3 py-2 last:border-none odd:bg-orange-100">
+        <li
+          key={comment.id}
+          className="border-b px-3 py-2 last:border-none odd:bg-orange-100"
+        >
           <div className="flex gap-3 pb-1 text-slate-500">
             <UserCircleIcon className="h-6 w-6" />
             {comment.user}
           </div>
-          <p className="italic">
-            {comment.message}
-          </p>
+          <p className="italic">{comment.message}</p>
         </li>
       ))}
     </ul>
