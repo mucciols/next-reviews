393d5b37b4e083fe84d49534737d0cc4041ba548
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-15 16:49:17 +0100

    eliminazione dei commenti

--- app/reviews/[slug]/actions.js
+++ app/reviews/[slug]/actions.js
@@ -4,14 +4,10 @@ import { createComment } from "@/lib/commets";
 
 export async function createCommentAction(formData) {
   
-  console.log('creazione del commento, prima del check degli errori');
-  
   if (!formData.get("user")) {
     return { isError: true, message: "Name field is required" };
   }
 
-  console.log('creazione del commento, dopo  del check degli errori');
-  
   const data = {
     slug: formData.get("slug"),
     user: formData.get("user"),
--- components/CommentForm.jsx
+++ components/CommentForm.jsx
@@ -1,31 +1,8 @@
 "use client";
 
 import { createCommentAction } from "@/app/reviews/[slug]/actions";
-import { useState } from "react";
+import { useFormState } from "@/lib/hooks";
 import { useFormStatus } from "react-dom";
-
-function useFormState(action, successAction) {
-
-  const [state, setState] = useState({ loading: false, error: null });
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setState({ loading: true, error: null });
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const result = await action(formData);
-    if (result?.isError) {
-      setState({ loading: false, error: result.message });
-    } else {
-      form.reset();
-      setState({ loading: false, error: null });
-      if(successAction)
-        successAction();
-    }
-  };
-
-  return [state, handleSubmit];
-}
 
 export default function CommentForm({ slug, title, notifySubmitComment }) {
   
