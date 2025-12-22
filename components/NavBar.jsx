6b4045039712919f715b4b2c43b77124c9365ee1
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-22 15:54:19 +0100

    comit

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,40 +1,45 @@
-import { cookies } from 'next/headers';
-import NavLink from './NavLink';
+import { cookies } from "next/headers";
+import NavLink from "./NavLink";
+import { jwtVerify } from "jose";
+
+const JWT_SECRET = new TextEncoder().encode("some-random-string");
+
+async function getUserFromSession() {
+  const sessionTokenCookie = (await cookies()).get("sessionToken");
+  if (sessionTokenCookie) {
+    try {
+      const { payload } = await jwtVerify(sessionTokenCookie.value, JWT_SECRET);
+      return payload;
+    } catch (error) {
+      console.warn('Invalid JWT' , error);
+    }
+  }
+}
 
 export default async function NavBar() {
-
-  const userCookie = (await cookies()).get('user');
-  const user = userCookie ? JSON.parse(userCookie.value) : null;
-
+  const user = await getUserFromSession();
   return (
     <nav>
       <ul className="flex gap-2">
         <li>
           {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/">Indie Gamer</Link> */}
-          <NavLink href="/" >Indie Gamer</NavLink>
+          <NavLink href="/">Indie Gamer</NavLink>
         </li>
-        <li className='ml-auto'>
+        <li className="ml-auto">
           {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/reviews">Reviews</Link> */}
-          <NavLink href="/reviews" >
-            Reviews
-          </NavLink>
+          <NavLink href="/reviews">Reviews</NavLink>
         </li>
         <li>
           {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/about" prefetch={false}>About</Link> */}
-          <NavLink href="/about">
-            About
-          </NavLink>
+          <NavLink href="/about">About</NavLink>
         </li>
-        {
-          user ? (
-            <li>{user.email}</li>
-          ) : (
+        {user ? (
+          <li>{user.email}</li>
+        ) : (
           <li>
-            <NavLink href="/sign-in">
-              Sign in
-            </NavLink>
-          </li>)
-        }
+            <NavLink href="/sign-in">Sign in</NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
