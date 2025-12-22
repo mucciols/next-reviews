c610d9524993e2944c1333902f2da306bafd8d12
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-22 21:15:20 +0100

    ass

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,15 +8,12 @@ export default async function NavBar() {
     <nav>
       <ul className="flex gap-2">
         <li>
-          {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/">Indie Gamer</Link> */}
           <NavLink href="/">Indie Gamer</NavLink>
         </li>
         <li className="ml-auto">
-          {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/reviews">Reviews</Link> */}
           <NavLink href="/reviews">Reviews</NavLink>
         </li>
         <li>
-          {/* <Link className='font-orbitron text-orange-800 hover:underline' href="/about" prefetch={false}>About</Link> */}
           <NavLink href="/about">About</NavLink>
         </li>
         {user ? (
